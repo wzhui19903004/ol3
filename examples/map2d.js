0b4edfa506cfa360b0c36cf28bc1a6e480699bb9
Tom Payne <twpayne@gmail.com>
2013-04-04 14:05:32 +0200

    Add ol.Map2D example

---
+++ examples/map2d.js
@@ -0,0 +1,17 @@
+goog.require('ol.Map2D');
+goog.require('ol.RendererHints');
+goog.require('ol.layer.TileLayer');
+goog.require('ol.source.OpenStreetMap');
+
+
+var map = new ol.Map2D({
+  center: [0, 0],
+  layers: [
+    new ol.layer.TileLayer({
+      source: new ol.source.OpenStreetMap()
+    })
+  ],
+  renderers: ol.RendererHints.createFromQueryData(),
+  target: 'map',
+  zoom: 2
+});
